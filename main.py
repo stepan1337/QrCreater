9359632504661d6677d05271f70eb7acd7fc0a19
Stepan <45758009+stepan1337@users.noreply.github.com>
2023-06-20 22:38:53 +0300

    QrCreater.py

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-from tkinter import*
+import qrcode
 
-root = Tk()
+text = input("Your Text: ")
+img = qrcode.make(text)
 
-root.title("Qr Creater")
-root.geometry("400x500")
-root.iconname()
-root["bg"] = "silver"
-
-def get():
-    label1["text"] = e.get()
-
-btn1 = Button(root, text= "Create", font=90,command=get)
-btn1.pack(side=BOTTOM,padx=40)
-
-label1 = Label(root,bg="silver",fg="black",font=120)
-label1.pack(pady=5)
-
-e = Entry(root,font=90,fg="black")
-e.pack()
-
-e.insert(0,"Введите текст")
-
-root.mainloop()
+print("Name Qr Code: ")
+img.save(input()+ ".png")
