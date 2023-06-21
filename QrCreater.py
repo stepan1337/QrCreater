f008f23fba6e26ffb9220086b382ad3c7c307937
Stepan <45758009+stepan1337@users.noreply.github.com>
2023-06-21 20:43:16 +0300

    Update QrCreater.py

--- QrCreater.py
+++ QrCreater.py
@@ -1,7 +1,18 @@
 import qrcode
+from plyer import notification
+import time
+import sys
 
 text = input("Your Text: ")
 img = qrcode.make(text)
 
 print("Name Qr Code: ")
 img.save(input()+ ".png")
+
+notification.notify(message="Qr Code находится в папке с программой",
+                          app_name="Qr Creater",
+                          title="Qr code создан!",
+                          app_icon="C:/Users/stepan/Desktop/qr Generated/venv/ico.ico")
+
+print("Нажмите любую клавишу для закрытия")
+input()
